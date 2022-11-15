8cee4b4353439227760ca27b694e4350706211cd
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2022-11-15 11:26:54 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/Link';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Link></Link>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
