117d3568f9c264574982cf8347d7eb5cc15d8f17
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2022-11-15 17:01:28 +0000

    1 file modified

--- pages/coins/index.js
+++ pages/coins/index.js
@@ -1,11 +1,18 @@
+import axios from 'axios';
+
 const CoinList = () => {
   return <div></div>;
 };
 
 getStaticProps;
 export const getStaticProps = async () => {
-
-  
-
+  const data = await axios.get(
+    'https://api.coinstats.app/public/v1/coins?skip=0'
+  );
+  return {
+    props: {
+      coindData: data.data,
+    },
+  };
 };
 export default CoinList;
