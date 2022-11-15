94eb3f4402bc053a9823eb641071ea8cb5365f65
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2022-11-15 12:28:18 +0000

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        
+        <title>Pedrotech Next Tutorial</title>
       </Head>
       <Link href="/About"> About</Link>
       <h1>Hello world</h1>
