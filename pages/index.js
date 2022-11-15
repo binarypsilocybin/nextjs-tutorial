75c35ad98e7ff417ab11aec2965b58639606d456
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2022-11-15 11:47:40 +0000

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,5 @@
-import Link from 'next/Link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Link></Link>
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <div className={styles.container}></div>;
 }
